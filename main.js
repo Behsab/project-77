60a18dc04368a2c12c6a22eec447d4edb0986408
Behsab <87884840+Behsab@users.noreply.github.com>
2022-01-28 20:16:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("I am testing out the console")
+var firstname="Ehsan";
+var lastname="Choudhury";
+var both=firstname.concat(lastname);
+console.log(both);
+var a = 10;
+var b = 5;
+var add =Math.add (a,b);
+console.log(add);
